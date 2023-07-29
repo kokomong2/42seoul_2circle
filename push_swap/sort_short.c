2d8372b45884a60a881f100ec4c16ee27649299e
Seung Jun Go <sgo@cx1r4s6.42seoul.kr>
2023-07-29 18:29:44 +0900

    10개 이하 sort_short 조건 추가

--- push_swap/sort_short.c
+++ push_swap/sort_short.c
@@ -6,7 +6,7 @@
 /*   By: sgo <sgo@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 20:44:34 by sgo               #+#    #+#             */
-/*   Updated: 2023/07/29 16:31:37 by sgo              ###   ########.fr       */
+/*   Updated: 2023/07/29 18:27:02 by sgo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,16 @@
 
 void	sort_short(t_info *info)
 {
+	int	top;
+	int	bottom;
+
+	bottom = info->size_a;
 	while (info->size_a > 3)
-		ft_push_b(info);
+	{
+		top = info->stack_a[info->size_a - 1];
+		if (top < bottom && top >= bottom - 3)
+			ft_ra(info);
+		else
+			ft_push_b(info);
+	}
 }
